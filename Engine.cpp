289adc7b785d8d14efe6e6fb6f2feebc79e96015
Your Name <you@example.com>
2025-12-10 18:43:15 -0800

    Partner: Tejbir Sidhu

--- Engine.cpp
+++ Engine.cpp
@@ -1,23 +1,26 @@
 #include "Engine.h"
 #include <SFML/Graphics.hpp>
 #include <SFML/System/Clock.hpp>
-#include <SFML/System/Time.hpp>
+#include <SFML/Window.hpp>
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+
 using namespace sf;
 using namespace std;
 
-Engine::Engine()
+
+Engine:: Engine()
 {
-    int width = VideoMode::getDesktopMode().width;
-    int height = VideoMode::getDesktopMode().height;
+    size_t width = VideoMode::getDesktopMode().width;
+    size_t height = VideoMode::getDesktopMode().height;
     VideoMode vm(width, height);
-    m_Window.create(vm, "Particles.exe", Style::Default);
+    m_Window.create(vm, "Particles.exe", Style:: Default);
 }
 
-void Engine::run()
+void Engine ::run()
 {
+    srand(time(0));
     Clock clock;
     Time time1 = clock.getElapsedTime();
 
@@ -26,19 +29,19 @@ void Engine::run()
     p.unitTests();
     cout << "Unit tests complete.  Starting engine..." << endl;
 
-    while (m_Window.isOpen())
+    while(m_Window.isOpen())
     {
         Time time2 = clock.restart();
         float seconds = time2.asSeconds();
         input();
         update(seconds);
         draw();
-    }
-}
 
-void Engine::input()
-{
-    srand(time(0));
+    }
+ }
+
+ void Engine:: input()
+ {
     Event event;
     while(m_Window.pollEvent(event))
     {
@@ -46,18 +49,16 @@ void Engine::input()
         {
             m_Window.close();
         }
-            
-        if(event.type == Event::MouseButtonPressed)
+        
+        if(event.type == Event:: MouseButtonPressed)
         {
-            Vector2i location = {event.mouseButton.x, event.mouseButton.y};
-            if(event.mouseButton.button == Mouse::Left)
+            srand(time(0));
+            Vector2i location= {event.mouseButton.x, event.mouseButton.y};
+            for(size_t i = 0; i < 5; i++)
             {
-                for (size_t i = 0; i < 5; i++)
-                {
-                    int numPoints = (rand() % 51) + 25;
-                    Particle particle(m_Window, numPoints, location);
-                    m_particles.push_back(particle); 
-                }
+                int numPoints = (rand() % 51) + 25; 
+                Particle particle(m_Window, numPoints, location);
+                m_particles.push_back(particle);
             }
         }
 
@@ -66,39 +67,32 @@ void Engine::input()
             m_Window.close();
         }
     }
-}
+ }
 
-void Engine::update(float dtAsSeconds)
+void Engine:: update(float dtAsSeconds)
 {
-    for (size_t i = 0; i < m_particles.size(); )
+    for(size_t i = 0; i < m_particles.size(); )
     {
-        if (m_particles[i].getTTL() == 0)
-        {
-            m_particles.erase(m_particles.begin() + i);
-        }
-
-        if (m_particles[i].getTTL() > 0)
+    
+        if(m_particles[i].getTTL() >= 0.0)
         {
             m_particles[i].update(dtAsSeconds);
             i++;
         }
-
         else
         {
             i = m_particles.erase(m_particles.begin() + i) - m_particles.begin();
         }
-    }    
+    }
 }
 
-void Engine::draw()
+void Engine:: draw()
 {
     m_Window.clear();
-
-    for (size_t i = 0; i < m_particles.size(); i++)
+    for(size_t i = 0; i < m_particles.size(); i++)
     {
         m_Window.draw(m_particles[i]);
+        
     }
-
     m_Window.display();
 }
-
--- Matrices.cpp
+++ Matrices.cpp
@@ -1,144 +1,123 @@
+//
+//  Matrices.cpp
+//  Matrices.cpp
+//
+//  Created by Tejbir Singh on 10/2/25.
+//
+
 #include "Matrices.h"
-#include <iomanip>
 
 namespace Matrices
 {
-	Matrix::Matrix(int _rows, int _cols)
-	{
-		rows = _rows;
-		cols = _cols;
-
-		a.resize(rows);
-
-		for (int i = 0; i < rows; i++)
-		{
-			a[i].resize(cols, 0);
-		}
-
-	}
-
-	Matrix operator+(const Matrix& a, const Matrix& b)
-	{
-		if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
-		{
-			throw runtime_error("Error: dimensions must agree");
-		}
-
-		Matrix c(a.getRows(), a.getCols());
-
-		for (int i = 0; i < a.getRows(); i++)
-		{
-			for (int j = 0; j < a.getCols(); j++)
-			{
-				c(i, j) = a(i, j) + b(i, j);
-			}
-		}
-
-		return c;
-	}
-
-	Matrix operator*(const Matrix& a, const Matrix& b)
-	{
-		double sum;
-
-		if (a.getCols() != b.getRows())
-		{
-			throw runtime_error("Error: dimensions must agree");
-		}
-
-		Matrix c(a.getRows(), b.getCols());
-
-
-		for (int k = 0; k < b.getCols(); k++)
-		{
-			for (int i = 0; i < a.getRows(); i++)
-			{
-				sum = 0;
-
-				for (int j = 0; j < a.getCols(); j++)
-				{
-					sum += a(i, j) * b(j, k);
-				}
-
-				c(i, k) = sum;
-			}
-		}
-
-		return c;
-	}
-
-	bool operator==(const Matrix& a, const Matrix& b)
-	{
-		if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
-		{
-			return false;
-		}
-
-		for (int i = 0; i < a.getRows(); i++)
-		{
-			for (int j = 0; j < a.getCols(); j++)
-			{
-				if (abs(a(i, j) - b(i, j)) >= 0.001)
-				{
-					return false;
-				}
-			}
-		}
-
-		return true;
-	}
-
-	bool operator!=(const Matrix& a, const Matrix& b)
-	{
-		if (a.getRows() != b.getRows() || a.getCols() != b.getCols())
-		{
-			return true;
-		}
-
-		for (int i = 0; i < a.getRows(); i++)
-		{
-			for (int j = 0; j < a.getCols(); j++)
-			{
-				if (abs(a(i, j) - b(i, j)) >= 0.001)
-				{
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	ostream& operator<<(ostream& os, const Matrix& a)
-	{
-		for (int i = 0; i < a.getRows(); i++)
-		{
-			for (int j = 0; j < a.getCols(); j++)
-			{
-				os << setw(10) << a(i, j) << " ";
-			}
-
-			os << " \n";
-
-			os << "\n";
-		}
-
-		return os;
-	}
-
-    RotationMatrix::RotationMatrix(double theta) : Matrix (2, 2)
+    Matrix::Matrix(int _rows, int _cols)
     {
-        a = {{cos(theta), -sin(theta)}, {sin(theta), cos(theta)}};
+        rows = _rows;
+        cols = _cols;
+        
+        a.resize(rows);
+        
+        for(int i = 0; i < rows; i++)
+        {
+            a[i].resize(cols,0.0);
+        }
+    }
+    
+    Matrix operator+(const Matrix& a, const Matrix& b)
+    {
+        if(a.getRows() != b.getRows() || a.getCols() != b.getCols())
+        {
+            throw runtime_error("Error: dimensions must agree");
+        }
+        Matrix c(a.getRows(), a.getCols());
+        for(int i = 0; i < a.getRows(); i++)
+        {
+            for(int j = 0; j < a.getCols(); j++)
+            {
+                c(i, j) = a(i,j) + b(i,j);
+            }
+        }
+        return c;
     }
 
-    ScalingMatrix::ScalingMatrix(double scale) : Matrix (2, 2)
+    Matrix operator*(const Matrix& a, const Matrix& b)
     {
-        a = {{scale, 0}, {0, scale}};
+        if(a.getCols() != b.getRows())
+        {
+            throw runtime_error("Error: dimensions must agree");
+        }
+        
+        Matrix c(a.getRows(), b.getCols());
+        
+        for(int k = 0; k < b.getCols(); k++)
+        {
+            for(int i = 0; i < a.getRows(); i++)
+            {
+                double sum = 0.0;
+                for(int j = 0; j < a.getCols(); j++)
+                {
+                    sum += a(i,j) *b(j,k);
+                }
+                c(i,k) = sum;
+            }
+        }
+        return c;
     }
 
-    TranslationMatrix::TranslationMatrix(double xShift, double yShift, int nCols) : Matrix (2, nCols)
+    bool operator==(const Matrix& a, const Matrix& b)
     {
-        for (int j = 0; j < nCols; j++)
+        if(a.getRows() != b.getRows() || a.getCols() != b.getCols())
+        {
+            return false;
+        }
+        
+        for(int i = 0; i < a.getRows(); i++)
+        {
+            for(int j = 0; j < a.getCols(); j++)
+            {
+                if(abs(a(i,j) - b(i,j)) >= 0.001) return false;
+            }
+        }
+        return true;
+    }
+
+    bool operator!=(const Matrix& a, const Matrix&b)
+    {
+        return !(a==b);  //since itll recognize that im calling operator== since im asking if their equal
+    }
+    
+    ostream& operator<<(ostream& os, const Matrix& a)
+    {
+        for(int i = 0; i < a.getRows(); i++)
+        {
+            for(int j = 0; j < a.getCols(); j++)
+            {
+                os << setw(10) << a(i,j) << " ";
+            }
+            os << endl;
+        }
+        
+        return os;
+    }
+
+    RotationMatrix::RotationMatrix(double theta) :Matrix(2,2)
+    {
+        a = {
+            {cos(theta), -sin(theta)},
+            {sin(theta), cos(theta)}
+        };
+    }
+
+    ScalingMatrix::ScalingMatrix(double scale) : Matrix(2,2)
+    {
+        a = {
+            {scale, 0},
+            {0, scale}
+        };
+    }
+
+    TranslationMatrix:: TranslationMatrix(double xShift, double yShift, int nCols) : Matrix(2, nCols)
+    {
+        for(int j = 0; j < nCols; j++)
         {
             a[0][j] = xShift;
             a[1][j] = yShift;
